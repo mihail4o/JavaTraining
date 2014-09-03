1fdec8ae8f22009df76f80494564e46654605be9
Mihail Mihov <mihail4o@gmail.com>
2014-09-02 19:54:04 -0700

    OOP Project first Git add.

---
+++ OOP/src/Car.java
@@ -0,0 +1,16 @@
+
+public class Car {
+	String color;
+	int numberOfDoors;
+	
+	boolean startEngine(){
+		// Code
+		return true;
+	}
+	
+	boolean stopEngine(){
+		
+		return false;
+	}
+
+}
---
+++ OOP/src/WeatherReport.java
@@ -0,0 +1,7 @@
+
+public class WeatherReport {
+	static double convertToCelsius(double far){
+		return ((far -32) * 5 / 9);
+	}
+
+}
